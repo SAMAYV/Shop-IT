d24a7b1cba7f9fd5ef40f0ffefe3ddb9efdd0bdb
SAMAYV <samay@iitg.ac.in>
2021-07-07 01:06:37 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+const connectDatabase = require("./config/database");
+
+const dotenv = require("dotenv");
+
+// setting up config file
+dotenv.config({ path: "backend/config/config.env" });
+
+// connecting to database
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});

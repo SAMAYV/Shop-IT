282511dc9ccd196d71afa2fea93890fbc5cbda3e
SAMAYV <samay@iitg.ac.in>
2021-07-07 14:13:17 +0530

    Handled Validation & Cast Errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -15,6 +15,19 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "PRODUCTION") {
     let error = { ...err };
     error.message = err.message;
+
+    // Wrong Mongoose Object ID Error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling Mongoose Validation Error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",

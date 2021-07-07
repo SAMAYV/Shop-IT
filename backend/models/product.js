f6ea941b46c24b6b9cdbb432e17efd70b9f49eda
SAMAYV <samay@iitg.ac.in>
2021-07-07 12:36:01 +0530

    Added seeding of products

--- backend/models/product.js
+++ backend/models/product.js
@@ -39,7 +39,7 @@ const productSchema = new mongoose.Schema({
       values: [
         "Electronics",
         "Cameras",
-        "Laptop",
+        "Laptops",
         "Accessories",
         "Headphones",
         "Food",

980fcdbac4677480c8b12b070e3e42ac3650584c
SAMAYV <samay@iitg.ac.in>
2021-07-07 18:51:42 +0530

    User authenticated or not

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -9,7 +9,9 @@ const {
   deleteProduct,
 } = require("../controllers/productController");
 
-router.route("/products").get(getProducts);
+const { isAuthenticatedUser } = require("../middlewares/auth");
+
+router.route("/products").get(isAuthenticatedUser, getProducts);
 
 router.route("/product/:id").get(getSingleProduct);
 

549a7d82113bd5f2a1cd200ae588dcfacc984917
SAMAYV <samay@iitg.ac.in>
2021-07-07 13:09:27 +0530

    Added Delete and Update product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 
-// Create new product => /api/v1/product/new
+// Create new product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
@@ -33,5 +33,41 @@ exports.getSingleProduct = async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+  });
+};
+
+// Update Product => /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      sucess: false,
+      message: "Product not found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete Product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      sucess: false,
+      message: "Product not found",
+    });
+  }
+  await product.remove();
+  res.status(200).json({
+    success: true,
+    message: "Product is deleted",
   });
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,12 +5,16 @@ const {
   getProducts,
   newProduct,
   getSingleProduct,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
 
 router.route("/product/:id").get(getSingleProduct);
 
-router.route("/product/new").post(newProduct);
+router.route("/admin/product/new").post(newProduct);
+
+router.route("/admin/product/:id").put(updateProduct).delete(deleteProduct);
 
 module.exports = router;

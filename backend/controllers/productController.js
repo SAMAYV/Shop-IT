63c4f0a02edb4c3aeadd94eb7dbbb707235740d8
SAMAYV <samay@iitg.ac.in>
2021-07-07 19:40:45 +0530

    Adding reset password

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,6 +6,7 @@ const APIFeatures = require("../utils/apiFeatures");
 
 // Create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+  req.body.user = req.user.id;
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
--- backend/models/product.js
+++ backend/models/product.js
@@ -83,6 +83,11 @@ const productSchema = new mongoose.Schema({
       },
     },
   ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,

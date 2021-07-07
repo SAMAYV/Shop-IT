60f09116741e74f7b858fc3795feb0b45ce6c971
SAMAYV <samay@iitg.ac.in>
2021-07-07 15:15:28 +0530

    Added pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,14 +15,19 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get all products => /api/v1/products?keyword=
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
+  const resPerPage = 4;
+  const productCount = await Product.countDocuments();
+
   const apiFeatures = new APIFeatures(Product.find(), req.query)
     .search()
-    .filter();
+    .filter()
+    .pagination(resPerPage);
   const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
     count: products.length,
+    productCount,
     products,
   });
 });

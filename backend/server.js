eb96c45dbffc8020273017f22c8e4a4c9f8c87d9
SAMAYV <samay@iitg.ac.in>
2021-07-07 14:01:56 +0530

    Handled Uncaught exceptions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
+
 // setting up config file
 dotenv.config({ path: "backend/config/config.env" });
 

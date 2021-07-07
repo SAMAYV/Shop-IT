091105901241ae5d9af6f348a3178f5e30d9ee81
SAMAYV <samay@iitg.ac.in>
2021-07-07 23:49:36 +0530

    Token and duplicate email prod error handled

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -28,6 +28,24 @@ module.exports = (err, req, res, next) => {
       error = new ErrorHandler(message, 400);
     }
 
+    // Handling Mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+      const message = "JSON Web Token is invalid. Try Again!!!";
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling expired JWT error
+    if (err.name === "TokenExpiredError") {
+      const message = "JSON Web Token is expired. Try Again!!!";
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",

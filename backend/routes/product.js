251e3accddc17e71cbcbf40f5e5996eda2b9f488
SAMAYV <samay@iitg.ac.in>
2021-07-07 19:16:31 +0530

    User Authorization

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -9,17 +9,19 @@ const {
   deleteProduct,
 } = require("../controllers/productController");
 
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/products").get(getProducts);
 
 router.route("/product/:id").get(getSingleProduct);
 
-router.route("/admin/product/new").post(isAuthenticatedUser, newProduct);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
 
 router
   .route("/admin/product/:id")
-  .put(isAuthenticatedUser, updateProduct)
-  .delete(isAuthenticatedUser, deleteProduct);
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
 module.exports = router;

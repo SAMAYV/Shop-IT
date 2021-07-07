db8d1c3904ceac54ea0e4fb42352e8d52b2bb761
SAMAYV <samay@iitg.ac.in>
2021-07-07 13:58:31 +0530

    Handled Promise rejections

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,17 @@ dotenv.config({ path: "backend/config/config.env" });
 // connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+// Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });

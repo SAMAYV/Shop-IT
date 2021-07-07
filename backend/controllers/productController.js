2f3c773fef64e74ef0a3ac3ce6620bc3f55225fe
SAMAYV <samay@iitg.ac.in>
2021-07-07 12:54:22 +0530

    Get single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,9 +9,29 @@ exports.newProduct = async (req, res, next) => {
   });
 };
 
-exports.getProducts = (req, res, next) => {
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
+
   res.status(200).json({
     success: true,
-    message: "This route will show all products in database",
+    count: products.length,
+    products,
+  });
+};
+
+// Get single product details => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      sucess: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
   });
 };

e6c96cb59687dfa713d6d850464dc3dbfd7afaf7
SAMAYV <samay@iitg.ac.in>
2021-07-07 19:01:14 +0530

    User authentication done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -45,3 +45,16 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+// Logout user => /api/v1/logout
+exports.logout = catchAsyncError(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});

78332adc4f58fca3e55ec5c76585d12c6a674584
SAMAYV <samay@iitg.ac.in>
2021-07-07 18:38:33 +0530

    Cookie & Token added for Authorization

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -16,10 +17,31 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
     },
   });
 
-  const token = user.getJwtToken();
+  sendToken(user, 200, res);
+});
 
-  res.status(201).json({
-    success: true,
-    token,
-  });
+// Login User => /api/v1/login
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Checks if email and password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  // Check if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  sendToken(user, 200, res);
 });

220702ef8dcc782810d3e9f6f9f91d066963000e
SAMAYV <samay@iitg.ac.in>
2021-07-09 01:32:29 +0530

    Custom Title added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from "react";
 
+import MetaData from "./layout/MetaData";
+
 const Home = () => {
   return (
     <Fragment>
+      <MetaData title={`Buy Best Products Online`} />
       <h1 id="products_heading">Latest Products</h1>
 
       <section id="products" className="container mt-5">

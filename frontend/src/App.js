e640795022c4f54b3dd7cc21571c146a07a470a7
SAMAYV <samay@iitg.ac.in>
2021-07-11 12:53:24 +0530

    Added Protected Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Login from "./components/user/Login";
 import Register from "./components/user/Register";
 import Profile from "./components/user/Profile";
 
+import ProtectedRoute from "./components/route/ProtectedRoute";
 import { loadUser } from "./actions/userActions";
 import store from "./store";
 
@@ -29,7 +30,7 @@ function App() {
 
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
-          <Route path="/me" component={Profile} exact />
+          <ProtectedRoute path="/me" component={Profile} exact />
         </div>
         <Footer />
       </div>
